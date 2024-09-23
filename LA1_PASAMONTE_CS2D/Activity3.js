cf5240b645fb785e11029d40fcfcfca5aa58c90b
clairepasamonte <clairepasamonte565@gmail.com>
2024-09-23 08:18:06 +0800

    Create Activity3.js

---
+++ LA1_PASAMONTE_CS2D/Activity3.js
@@ -0,0 +1,9 @@
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 9;
+let favNumbPlusUndefined = favNumb + undefined;
+
+console.log("Is JavaScript a progamming language?", isJsProgrammingLanguage);
+console.log("Is JavaScript Hard?", isJsHard);
+console.log("Favorite Number:", favNumb);
+console.log("Favorite number plus undefined:", favNumbPlusUndefined);
