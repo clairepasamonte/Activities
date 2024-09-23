72850facd06faa0d9561fb3194ae4b4c176b9d9e
clairepasamonte <clairepasamonte565@gmail.com>
2024-09-23 09:53:07 +0800

    Create Activity5.js

---
+++ LA1_PASAMONTE_CS2D/Activity5.js
@@ -0,0 +1,14 @@
+// create variable for firstname and lastname
+let favActorFirstName = "Jason "; 
+let favActorLastName = "Mamoa";
+let fullName = favActorFirstName + favActorLastName; // concatenate the favActorFirstName and favActorLastName
+let uppercase = fullName.toUpperCase(); //CAPITALZE fav actor's name
+let message = "My Favorite Actor is " + uppercase + " He is a famous american actor and my most favorite." //stores message and the name should be uppercase
+message += " His best show is Silicon Valley."; // append the text to message variable
+
+//log all the variables in console using code runner
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(message);
