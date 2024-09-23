fb3036548bbe7d680302a345b842d65903ef62c7
clairepasamonte <clairepasamonte565@gmail.com>
2024-09-23 08:45:06 +0800

    Create activity4.js

---
+++ LA1_PASAMONTE_CS2D/activity4.js
@@ -0,0 +1,13 @@
+// create variable names to store your favorite numbers using firstFavNumb and secondFavNumb.
+let firstFavNumb = 9;
+let secondFavNumb = 1;
+
+// log all the variables to the console using code runner.
+console.log(firstFavNumb>secondFavNumb);
+console.log(firstFavNumb<secondFavNumb);
+console.log(firstFavNumb>=secondFavNumb);
+console.log(firstFavNumb<=secondFavNumb);
+console.log(firstFavNumb===secondFavNumb);
+console.log(firstFavNumb==secondFavNumb);
+console.log(firstFavNumb!==secondFavNumb);
+console.log(firstFavNumb!=secondFavNumb);
