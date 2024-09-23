419f7b6b0fce860f05125fa5028e3aa064691b5c
clairepasamonte <clairepasamonte565@gmail.com>
2024-09-23 09:58:44 +0800

    Update Activity2.js

--- LA1_PASAMONTE_CS2D/Activity2.js
+++ LA1_PASAMONTE_CS2D/Activity2.js
@@ -1,9 +1,10 @@
+//numbers
 let firstFavNum = 9;
 let secondFavNum = 1;
 
-console.log(firstFavNum+secondFavNum);
-console.log(firstFavNum-secondFavNum);
-console.log(firstFavNum*secondFavNum);
-console.log(firstFavNum/secondFavNum);
-console.log(firstFavNum&secondFavNum);
-console.log(firstFavNum**secondFavNum);
+console.log(firstFavNum+secondFavNum); //add the firstFavNum and the secondFavNum
+console.log(firstFavNum-secondFavNum); // subtract the firstFavNum and the secondFavNum
+console.log(firstFavNum*secondFavNum); // multiply the firstFavNum and the secondFavNum
+console.log(firstFavNum/secondFavNum); // divide the firstFavNum and the secondFavNum
+console.log(firstFavNum&secondFavNum); // mod the firstFavNum and the secondFavNum
+console.log(firstFavNum**secondFavNum); // the power of firstFavNum and the secondFavNum
