5b859f1db9fde9ff27cb02d11f0afecfea8ac788
clairepasamonte <clairepasamonte565@gmail.com>
2024-09-23 08:05:27 +0800

    Delete Activity2.js

--- Activity2.js
+++
@@ -1,9 +0,0 @@
-let firstFavNum = 9;
-let secondFavNum = 1;
-
-console.log(firstFavNum+secondFavNum);
-console.log(firstFavNum-secondFavNum);
-console.log(firstFavNum*secondFavNum);
-console.log(firstFavNum/secondFavNum);
-console.log(firstFavNum&secondFavNum);
-console.log(firstFavNum**secondFavNum);
