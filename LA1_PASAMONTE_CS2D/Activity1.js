93cd215d4ecfd0d15716a6c794405f51ac895b1f
clairepasamonte <clairepasamonte565@gmail.com>
2024-09-23 08:04:43 +0800

    Activity1.js

---
+++ LA1_PASAMONTE_CS2D/Activity1.js
@@ -0,0 +1,9 @@
+let name = "CLaire Agwayaway Pasamonte";
+let whatDoYouWannaBecomeInYourLife = "Programmer";
+let gender = "Female";
+let facebookAcct = "Claire Pasamonte";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(facebookAcct);
