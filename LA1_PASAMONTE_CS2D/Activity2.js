5919f91919e2439bbdd048db09d30ecc3810c442
clairepasamonte <clairepasamonte565@gmail.com>
2024-09-23 08:05:54 +0800

    Create Activity2.js

---
+++ LA1_PASAMONTE_CS2D/Activity2.js
@@ -0,0 +1,9 @@
+let firstFavNum = 9;
+let secondFavNum = 1;
+
+console.log(firstFavNum+secondFavNum);
+console.log(firstFavNum-secondFavNum);
+console.log(firstFavNum*secondFavNum);
+console.log(firstFavNum/secondFavNum);
+console.log(firstFavNum&secondFavNum);
+console.log(firstFavNum**secondFavNum);
