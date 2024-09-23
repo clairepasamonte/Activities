7ffa9e13bab9ea977ae916ff02d37ee511731d21
clairepasamonte <clairepasamonte565@gmail.com>
2024-09-23 08:05:10 +0800

    Delete Activity1.js

--- Activity1.js
+++
@@ -1,9 +0,0 @@
-let name = "CLaire Agwayaway Pasamonte";
-let whatDoYouWannaBecomeInYourLife = "Programmer";
-let gender = "Female";
-let facebookAcct = "Claire Pasamonte";
-
-console.log(name);
-console.log(whatDoYouWannaBecomeInYourLife);
-console.log(gender);
-console.log(facebookAcct);
