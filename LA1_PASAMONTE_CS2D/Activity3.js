d729d84e4adcf2a72d33f3904bc58609f9778990
clairepasamonte <clairepasamonte565@gmail.com>
2024-09-23 09:59:55 +0800

    Update Activity3.js

--- LA1_PASAMONTE_CS2D/Activity3.js
+++ LA1_PASAMONTE_CS2D/Activity3.js
@@ -1,7 +1,8 @@
+// booleans
 let isJsProgrammingLanguage = true;
 let isJsHard = false;
 let favNumb = 9;
-let favNumbPlusUndefined = favNumb + undefined;
+let favNumbPlusUndefined = favNumb + undefined; // add the favNumb to undefined number
 
 console.log("Is JavaScript a progamming language?", isJsProgrammingLanguage);
 console.log("Is JavaScript Hard?", isJsHard);
